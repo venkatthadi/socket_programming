0403d5122233c3a25ea7696c9ca7f97b6424ad15
venkatthadi <venkat@mobicip.com>
2024-02-27 14:19:17 +0530

    27.02.24

--- C chat server with HTML-JS chat client/server.c
+++ C chat server with HTML-JS chat client/server.c
@@ -17,7 +17,8 @@
 
 char *GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"; //global unique identifier
 
-void Base64Encode(char *client_key, char *accept_key) { //Encodes a binary safe base 64 string
+//Encodes a binary safe base 64 string
+void Base64Encode(char *client_key, char *accept_key) { 
     char combined_key[1024];
     strcpy(combined_key, client_key);
     strcat(combined_key, GUID);
@@ -49,39 +50,92 @@ void Base64Encode(char *client_key, char *accept_key) { //Encodes a binary safe 
 }
 
 void send_message_to_client(int client_sockfd, uint8_t *payload, int payload_length){
-    int i, header_size = 2;
-    uint8_t frame[1024];
-    int mask = 0;
+    int header_size = 2;
+    uint8_t frame[10];
+    int mask = 0, fin = 1, opcode = 1;
 
-    frame[0] = (1 << 7) | (1 & 0x0F); //fin and opcode - 129
+    frame[0] = (fin << 7) | (opcode & 0x0F); //129(1000 0001)
     frame[1] = (mask << 7); //0
     if(payload_length <= 125){
         frame[1] |= payload_length;
     }else if(payload_length <= 65536){
         frame[1] |= 126;
-        frame[2] = (payload_length >>  8) && 0xFF;
-        frame[3] = (payload_length      ) && 0xFF;
+        frame[2] = (payload_length >>  8) & 0xFF;
+        frame[3] = (payload_length      ) & 0xFF;
         header_size += 2;
     }else{
-        frame[1] |= 121;
-        frame[2] = (payload_length >> 56) && 0xFF;
-        frame[3] = (payload_length >> 48) && 0xFF;
-        frame[4] = (payload_length >> 40) && 0xFF;
-        frame[5] = (payload_length >> 32) && 0xFF;
-        frame[6] = (payload_length >> 24) && 0xFF;
-        frame[7] = (payload_length >> 16) && 0xFF;
-        frame[8] = (payload_length >>  8) && 0xFF;
-        frame[9] = (payload_length      ) && 0xFF;
+        frame[1] |= 127;
+        frame[2] = (payload_length >> 56) & 0xFF;
+        frame[3] = (payload_length >> 48) & 0xFF;
+        frame[4] = (payload_length >> 40) & 0xFF;
+        frame[5] = (payload_length >> 32) & 0xFF;
+        frame[6] = (payload_length >> 24) & 0xFF;
+        frame[7] = (payload_length >> 16) & 0xFF;
+        frame[8] = (payload_length >>  8) & 0xFF;
+        frame[9] = (payload_length      ) & 0xFF;
         header_size += 8;
     }
-    memcpy(frame + header_size, payload, payload_length);
     
-    frame[header_size + payload_length] = '\0';
-    printf("%s\n", frame);
+    uint8_t response[header_size + payload_length];
+    int32_t i, respInd = 0;
+    for(i = 0; i < header_size; i++){
+        response[respInd++] = frame[i];
+    }
+    for(i = 0; i < payload_length; i++){
+        response[respInd++] = payload[i];
+    }
 
-    if(write(client_sockfd, frame, header_size + payload_length) > 0){
+    if(send(client_sockfd, response, header_size + payload_length, 0) > 0){
         printf("[+]message sent to client.\n");
     }
+}
+
+void decode_websocket_frame_header(uint8_t buffer[], int buffer_length){
+    //decode header for payload length and mask bit
+    int payload_length = buffer[1] & 0x7F;
+    // printf("payload length - %d\n", payload_length);
+    // printf("buffer length - %d\n", buffer_length);
+    uint8_t message[payload_length];
+    int mask_bit = buffer[1] && (1 << 7); //always 1
+    // printf("mask bit - %d\n", mask_bit);
+
+    //move forward until we reach mask
+    int indexFirstMask = 2;
+    if(payload_length == 126){
+        indexFirstMask = 4;
+    }else if(payload_length == 127){
+        indexFirstMask = 10;
+    }
+
+    //get mask
+    size_t i;
+    uint8_t mask[4];
+    for(i = 0; i < 4; i++){
+        mask[i] = buffer[indexFirstMask + i];
+    }
+
+    //move to the start of payload
+    i += indexFirstMask;
+    int ind = 0;
+    while(i++ < buffer_length){
+        message[ind] = buffer[indexFirstMask + 4 + ind] ^ mask[ind % 4];
+        ind++;
+    }
+    // printf("message length - %d\n", strlen(message));
+    message[ind] = '\0';
+    printf("received message - %s\n", message);
+}
+
+void recv_message_from_client(int client_sockfd){
+    int bytes_received;
+    uint8_t buffer[MAX_SIZE];
+    
+    memset(buffer, 0, MAX_SIZE);
+    if((bytes_received = recv(client_sockfd, buffer, MAX_SIZE, 0)) > 0){
+        printf("[+]message received.\n");
+    }
+    // printf("message received - \n%s\n", buffer);
+    decode_websocket_frame_header(buffer, strlen(buffer));
 }
 
 void handle_client(int cli_sockfd){
@@ -100,14 +154,15 @@ void handle_client(int cli_sockfd){
     char *key = strstr(buffer, "Sec-WebSocket-Key: ");
     char websoc_key[MAX_SIZE];
     memset(&websoc_key, '\0', MAX_SIZE);
-    sscanf(key, "Sec-WebSocket-Key: %s", websoc_key);
-    printf("[+]key - %s\n", websoc_key);
+    sscanf(key, "Sec-WebSocket-Key: %s", websoc_key);//extract the key
+    // printf("[+]key - %s\n", websoc_key);
 
     char hash[1024];
     Base64Encode(websoc_key, hash);
 
+    //send the response to upgrade the connection
     char response[MAX_SIZE];
-    sprintf(response, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: %s\r\n", hash);
+    sprintf(response, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: %s\r\n\r\n", hash);
     printf("response - \n%s\n", response);
     write(cli_sockfd, response, strlen(response));
     printf("[+]handshake established\n");
@@ -116,17 +171,8 @@ void handle_client(int cli_sockfd){
     char *payload = "hello, from server!!!";
     // printf("%s\n",payload);
     send_message_to_client(cli_sockfd, (uint8_t *) payload, strlen(payload));
-    
 
-    // bzero(buffer, MAX_SIZE);
-    // if((bytes_received = recv(cli_sockfd, buffer, MAX_SIZE, 0)) < 0){
-    //     perror("no message received");
-    //     return;
-    // }
-    // printf("[+]message received.\n");
-
-    // decode_websocket_frame_header();
-
+    recv_message_from_client(cli_sockfd);
 }
 
 int main(){
